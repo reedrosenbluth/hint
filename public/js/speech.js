39bd58d1fa5f44833dea1c061293387f8e5f2a02
Satya Bodduluri <kowabungappl@gmail.com>
2015-06-27 14:06:50 -0700

    new branch

--- public/js/speech.js
+++ public/js/speech.js
@@ -4,18 +4,30 @@ $(document).ready(function () {
     var final_transcript = '';
     recognition.continuous = true;
     recognition.interimResults = true;
-    //var interim_transcript = '';
+    var interim_transcript_final = '';
 
     recognition.onresult = function (event) {
       var interim_transcript = '';
       for (var i = event.resultIndex; i < event.results.length; ++i) {
-        console.log(event.results);
-        if (i === event.results.length - 1) {
-          console.log(event.results[i][0].transcript);
-          console.log(event.results[i][0].confidence);
-        }
+        // console.log(event.results);
+
+          var confidence = event.results[i][0].confidence;
+          if ((confidence > 0.85) && i === (event.results.length - 1)) {
+            
+            new_transcript = event.results[i][0].transcript;
+
+            if (new_transcript.startsWith(interim_transcript_final)) {
+              interim_transcript_final = new_transcript.substring(interim_transcript_final.length);
+            } else {
+              interim_transcript_final = new_transcript;
+            }
+
+            console.log(interim_transcript_final);
+          }
+          
         if (event.results[i].isFinal) {
           final_transcript += event.results[i][0].transcript;
+          interim_transcript_final = '';
         } else {
           interim_transcript += event.results[i][0].transcript;
         }
--- sockets/base.js
+++ sockets/base.js
@@ -1,6 +1,14 @@
+var nlp = require('nlp_compromise')
+
+function processSentence(sentence) {
+  return 'sentence';
+}
+
 module.exports = function (io) {
   // socket.io events
   io.on( "connection", function( socket ) {
       console.log( "A user connected" );
+
+      socket.on('new sentence', processSentence);
   });
 }
