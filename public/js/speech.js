ed15f8a1a4f5b56bbc401747251ab2e09c641034
Satya Bodduluri <kowabungappl@gmail.com>
2015-06-27 17:08:35 -0700

    Some server side stuff now

--- public/js/speech.js
+++ public/js/speech.js
@@ -1,10 +1,12 @@
+var socket = io('http://' + location.host);
+
 $(document).ready(function () {
   if ('webkitSpeechRecognition' in window) {
     var recognition = new webkitSpeechRecognition();
     var final_transcript = '';
     recognition.continuous = true;
     recognition.interimResults = true;
-    var interim_transcript_final = '';
+    var interim_result = '';
 
     recognition.onresult = function (event) {
       var interim_transcript = '';
@@ -12,22 +14,35 @@ $(document).ready(function () {
         // console.log(event.results);
 
           var confidence = event.results[i][0].confidence;
-          if ((confidence > 0.85) && i === (event.results.length - 1)) {
+          if ((confidence > 0.85) && i === (event.results.length - 1) && !event.results[i].isFinal) {
             
-            new_transcript = event.results[i][0].transcript;
+            new_result = event.results[i][0].transcript;
 
-            if (new_transcript.startsWith(interim_transcript_final)) {
-              interim_transcript_final = new_transcript.substring(interim_transcript_final.length);
+            if (new_result.startsWith(interim_result)) {
+              new_result = new_result.substring(interim_result.length);
+              interim_result += new_result;
+              
+              if (new_result !== '') {
+                new_result = $.trim(new_result);
+                socket.emit('new_result', { data: new_result });
+              }
+
+              console.log(new_result);
             } else {
-              interim_transcript_final = new_transcript;
-            }
+              interim_result = new_result;
 
-            console.log(interim_transcript_final);
+              if (new_result !== '') {
+                new_result = $.trim(new_result);
+                socket.emit('new_result', { data: new_result });
+              }
+
+              console.log(new_result)
+            }
           }
           
         if (event.results[i].isFinal) {
           final_transcript += event.results[i][0].transcript;
-          interim_transcript_final = '';
+          interim_result = '';
         } else {
           interim_transcript += event.results[i][0].transcript;
         }
@@ -37,8 +52,7 @@ $(document).ready(function () {
       $('#final_span').html(final_transcript);
     };
 
-    document.querySelector('button').addEventListener('click', function () {
-      recognition.start();
-    });
+    recognition.start();
+
   }
 });
--- sockets/base.js
+++ sockets/base.js
@@ -1,7 +1,18 @@
 var nlp = require('nlp_compromise')
 
-function processSentence(sentence) {
-  return 'sentence';
+var results = [];
+
+function processResult(result) {
+  var sample = result.data;
+  var entities = nlp.pos(sample).nouns();
+  console.log(entities);
+
+  entities.forEach(function(entity, index, array) {
+    console.log(entity.text);
+  })
+
+  results.push(sample);
+  console.log(results);
 }
 
 module.exports = function (io) {
@@ -9,6 +20,6 @@ module.exports = function (io) {
   io.on( "connection", function( socket ) {
       console.log( "A user connected" );
 
-      socket.on('new sentence', processSentence);
+      socket.on('new_result', processResult);
   });
 }
